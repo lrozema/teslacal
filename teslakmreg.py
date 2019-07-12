ebd2be67f7cab74ea33453f46b9885f5039727b5
Lourens Rozema <ik@lourensrozema.nl>
2019-07-12 20:53:11 +0200

    GPS loookup added.

--- teslakmreg.py
+++ teslakmreg.py
@@ -8,6 +8,7 @@
 
 from ConfigParser import RawConfigParser
 from googlesheet import GoogleSheet
+from geopy.geocoders import Nominatim
 
 import teslajson
 import string
@@ -24,6 +25,34 @@ def now():
 config = RawConfigParser()
 config.read('teslacal.cfg')
 
+# Connect to the Tesla cloud
+c = teslajson.Connection(config.get('tesla', 'username'), config.get('tesla', 'password'))
+
+# Filter the vehicles list based on VIN if specified
+va = c.vehicles
+vin = config.get('tesla', 'vin')
+if vin is not None:
+    va = [ v for v in va if v['vin'] == vin ]
+
+# Make sure we did find a vehicle and then select the first
+if not len(va):
+    raise Exception("Tesla not found!")
+v = va[0]
+
+print "Tesla "+v['display_name']+" found!"
+v.wake_up()
+
+vehicle_state = v.data_request('vehicle_state')
+drive_state = v.data_request('drive_state')
+
+if 0:
+    import json
+    print json.dumps({'vehicle_state': vehicle_state, 'drive_state': drive_state})
+
+odometer = float(vehicle_state['odometer']) * 1.60934
+in_motion = vehicle_state['is_user_present'] == '?'
+
+# Allow to overwrite driving data so that we can test the script
 if len(sys.argv) > 2:
     # Manual driving
     odometer = float(sys.argv[1])
@@ -31,32 +60,6 @@ if len(sys.argv) > 2:
 
     # Remove arguments
     sys.argv = sys.argv[:1]
-
-else:
-    # Connect to the Tesla cloud
-    c = teslajson.Connection(config.get('tesla', 'username'), config.get('tesla', 'password'))
-
-    # Filter the vehicles list based on VIN if specified
-    va = c.vehicles
-    vin = config.get('tesla', 'vin')
-    if vin is not None:
-        va = [ v for v in va if v['vin'] == vin ]
-
-    # Make sure we did find a vehicle and then select the first
-    if not len(va):
-        raise Exception("Tesla not found!")
-    v = va[0]
-
-    print "Tesla "+v['display_name']+" found!"
-    v.wake_up()
-
-    vehicle_state = v.data_request('vehicle_state')
-
-    import json
-    print json.dumps(vehicle_state)
-
-    odometer = float(vehicle_state['odometer']) * 1.60934
-    in_motion = vehicle_state['is_user_present'] == '?'
 
 # Get the current state
 data = shelve.open('kmreg.db')
@@ -67,11 +70,17 @@ if in_motion:
 
 # Did we not move?
 if odometer != data.get('Beginstand', None):
+    latitude = drive_state['latitude']
+    longitude = drive_state['longitude']
+
+    # Lookup current address
+    geolocator = Nominatim(user_agent='Tesla KM registration')
+    address = geolocator.reverse((latitude, longitude)).address
+
     # We moved, make a record
     data['Eindtijd'] = now()
     data['Eindstand'] = odometer
-    data['Begin adres'] = 'TODO'
-    data['Eind adres'] = 'TODO'
+    data['Eind adres'] = address
 
     # Get the spreadsheet id
     spreadsheet_id = config.get('google', 'spreadsheetId')
@@ -107,6 +116,9 @@ if odometer != data.get('Beginstand', None):
             } for k, v in data.items()
         ],
     })
+
+    # We don't want to geolocate too often, so keep this here
+    data['Begin adres'] = address
 
 # Keep track of a new start time
 data['Begintijd'] = now()
